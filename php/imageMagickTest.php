388bdf35ab649bd4805c91623b1964570cc577c5
marky edwards <mark@edwardsmark.com>
2025-02-18 09:36:31 -0700

    Update imageMagickTest.php

--- php/imageMagickTest.php
+++ php/imageMagickTest.php
@@ -11,7 +11,7 @@
             /* Start a new pattern called "gradient" */
       $draw->pushPattern('gradient', 0, 0, 50, 50);
             /* Composite the gradient on the pattern */
-      $draw->composite(Imagick::COMPOSITE_OVER, 0, 0, 50, 50, \$im);
+      $draw->composite(Imagick::COMPOSITE_OVER, 0, 0, 50, 50, $im);
             /* Close the pattern */
       $draw->popPattern();
             /* Use the pattern called "gradient" as the fill */
@@ -24,7 +24,7 @@
       $canvas = new Imagick();
       $canvas->newImage(350, 70, "white");
            /* Draw the ImagickDraw on to the canvas */
-      $canvas->drawImage(\$draw);
+      $canvas->drawImage($draw);
             /* 1px black border around the image */
       $canvas->borderImage('black', 1, 1);
             /* Set the format to PNG */
