686aaa49c55f50c2f65d34d801a9ea397320b552
marky edwards <mark@edwardsmark.com>
2025-07-29 10:12:55 -0700

    Create 05_test_perl_connection.pl

---
+++ mariadb/05_test_perl_connection.pl
@@ -0,0 +1,20 @@
+
+###   partially written from:  https://www.a2hosting.com/kb/developer-corner/mysql/connect-to-mysql-using-perl/
+use DBI;
+
+my $hostname = 'localhost' ;
+my $username = 'testUser'  ;
+my $password = 'password'  ;
+my $database = 'testDb'    ;
+
+my  $dbh = DBI->connect('DBI:mysql:' . $database . ':' . $hostname, $username, $password)
+      or die 'could not connect!';
+
+my $query = $dbh->prepare('SELECT VERSION();') or die 'could not prepare';
+
+$query->execute();
+
+my @resultArray =  $query->fetch()      ;
+print $resultArray[0][0] . "\n"         ;
+
+$query->finish();
