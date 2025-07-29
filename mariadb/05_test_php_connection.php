7a3a78eae056b3c4c5875c3c7228fb9c6a088aca
marky edwards <mark@edwardsmark.com>
2025-07-29 10:12:05 -0700

    Create 05_test_php_connection.php

---
+++ mariadb/05_test_php_connection.php
@@ -0,0 +1,30 @@
+<?php
+##
+##  05_test_php_connection.php    2025-07-29
+##
+
+$hostname = 'localhost' ;
+$username = 'testUser'  ;
+$password = 'password'  ;
+$database = 'testDb'    ;
+
+try {
+        $dbh = new PDO ("mysql:host=$hostname;dbname=$database", $username, $password);
+        echo 'Successfully connected to database' . "\n" ;
+
+        /*** set the error reporting attribute ***/
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        /*** prepare the SQL statement ***/
+        $stmt = $dbh->prepare("SELECT VERSION();");
+
+        $stmt->execute();
+        echo  implode(' ', $stmt->fetch()) . "\n"       ;
+        
+        /*** close the database connection ***/
+        $dbh = null;
+
+} catch(PDOException $e) {
+        echo $e->getMessage() . "\\n"   ;
+}
+?>
