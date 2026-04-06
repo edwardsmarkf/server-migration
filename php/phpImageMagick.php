920ce686cfcc55281a2a09fb1f3624c5a4794f6e
marky edwards <mark@edwardsmark.com>
2026-04-06 16:31:57 -0700

    Create phpImageMagick.php

---
+++ php/phpImageMagick.php
@@ -0,0 +1,9 @@
+
+
+<?php
+    $image = new Imagick();
+    $image->newImage(100, 100, new ImagickPixel('red'));
+    $image->setImageFormat('png');
+    header('Content-type: image/png');
+    echo $image;
+?>
